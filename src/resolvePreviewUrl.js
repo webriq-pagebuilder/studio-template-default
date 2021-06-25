f245e8a4aa7976dcddab7479a0f661188480c99c
Dorell James <galangdj@gmail.com>
2021-06-25 21:41:45 +0800

    make SANITY_STUDIO_DEV_SITE_URL optional and default to Next app dev server

--- src/resolvePreviewUrl.js
+++ src/resolvePreviewUrl.js
@@ -1,7 +1,9 @@
 export default function resolveProductionUrl(document) {
   const previewUrl = `api/preview?secret=${process.env.SANITY_STUDIO_PREVIEW_SECRET}&slug=${document.slug.current}`;
   if (window.location.hostname.includes("localhost")) {
-    return `${process.env.SANITY_STUDIO_DEV_SITE_URL}/${previewUrl}`;
+    return `${
+      process.env.SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"
+    }/${previewUrl}`;
   }
 
   return `${process.env.SANITY_STUDIO_PRODUCTION_SITE_URL}/${previewUrl}`;
