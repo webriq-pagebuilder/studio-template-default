d244814712b16126371364b3d9c57acfb76716a8
Ronix Otom <ronaldo.otom@webriq.services>
2021-08-26 10:13:52 +0800

    removing uneccessary files

--- plugins/webriq-payments/index.js
+++
@@ -1,9 +0,0 @@
-import WebriQPayments from './WebriQPayments'
-import WebriQPaymentsIcon from './WebriQPaymentsIcon'
-
-export default {
-  title: 'WebriQ Payments',
-  name: 'webriq-payments',
-  icon: WebriQPaymentsIcon,
-  component: WebriQPayments
-}
