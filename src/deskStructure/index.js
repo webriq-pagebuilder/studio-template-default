cae87e89142da30c1d172d7e91dbdb7f4b9479e4
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2021-09-29 22:13:43 +0800

    remove blog posts from deskStructure

--- src/deskStructure/index.js
+++ src/deskStructure/index.js
@@ -1,17 +1,12 @@
 import S from "@sanity/desk-tool/structure-builder";
-
 import pages from "./pages";
-import posts from "@webriq-pagebuilder/sanity-plugin-schema-blog/lib/deskStructure";
 
-const blogs = Object.values(posts);
 const hiddenTypes = ["media.tag", "category", "page", "post", "author"];
-
 export default () =>
   S.list()
     .title("Content")
     .items([
       pages,
-      S.listItem(...blogs),
       ...S.documentTypeListItems().filter(
         (listItem) => !hiddenTypes.includes(listItem.getId())
       ),
