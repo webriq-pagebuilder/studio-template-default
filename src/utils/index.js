2494c5020b8dbc6402c6d103ef117fa789d194c9
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2022-09-27 20:29:00 +0800

    Add sanity-plugin-schema-commerce

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 /* 
   Call this functional component to update the existing schemas from included plugins with the changes from the custom folder
 */
+import { SANITY_STUDIO_IN_CSTUDIO } from "../config";
 
 export const mergeReplaceAndAdd = (existingItems, newItems) => {
   const updatedItems = existingItems.map((existingItem) => {
@@ -18,6 +19,10 @@ export const mergeReplaceAndAdd = (existingItems, newItems) => {
   const additionalSchemas = newItems.reduce((all, current) => {
     if (!existingItems.map((i) => i.name).includes(current.name)) {
       all = [...all, current];
+    }
+
+    if (SANITY_STUDIO_IN_CSTUDIO === "false") {
+      return all?.map((items) => ({ ...items, readOnly: true }));
     }
 
     return all;
