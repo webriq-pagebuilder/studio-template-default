53c95a41d5963c53d2a1a63dbb960171ee57c0ce
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2022-11-16 15:49:19 +0800

    Updates on when to display C-Studio components

--- src/deskStructure/index.js
+++ src/deskStructure/index.js
@@ -2,6 +2,10 @@ import S from "@sanity/desk-tool/structure-builder";
 import { sections } from "../badges/sectionBadge";
 import pages from "./pages";
 import store from "./store/";
+import { SANITY_STUDIO_IN_CSTUDIO, SANITY_PROJECT_DATASET } from "../config";
+
+const isProductionDataset = SANITY_PROJECT_DATASET === "production";
+const hidden = isProductionDataset && SANITY_STUDIO_IN_CSTUDIO === "false";
 
 const hiddenTypes = [
   "media.tag",
@@ -18,14 +22,19 @@ const hiddenTypes = [
   "searchPage",
   ...sections,
 ];
+
+const defaultListItems = [
+  pages,
+  ...S.documentTypeListItems().filter(
+    (listItem) => !hiddenTypes.includes(listItem.getId())
+  ),
+];
+
 export default () =>
   S.list()
     .title("Content")
-    .items([
-      pages,
-      S.divider(),
-      store,
-      ...S.documentTypeListItems().filter(
-        (listItem) => !hiddenTypes.includes(listItem.getId())
-      ),
-    ]);
+    .items(
+      hidden // Hide STORE on "production" dataset and C-Studio is disabled
+        ? defaultListItems
+        : [...defaultListItems, S.divider(), store] // Show STORE on "production" OR "staging" dataset and C-Studio is enabled
+    );
