4eb995e43ccb6731672180bb2d2fab37d9566b1f
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-02-23 18:59:14 +0800

    Update sanity packages to latest including webriq-payments in V3

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,27 +1,36 @@
 import { defineConfig } from "sanity"
-import { visionTool } from "@sanity/vision"
-import { schemaTypes } from "./schemas/schema"
-import deskStructure from "./src/deskStructure"
-import { Logo } from "./src/brand/logo"
-import { LiveURLBadge } from "./src/badges/LiveURLBadge"
-import { SectionBadge } from "./src/badges/sectionBadge"
 import {
   SANITY_STUDIO_PROJECT_NAME,
   SANITY_STUDIO_DATASET,
   SANITY_STUDIO_API_PROJECT_ID,
 } from "./src/config"
+
+// desk customization
+import deskStructure from "./src/deskStructure"
+import { Logo } from "./src/brand/logo"
+import { DefaultStudioTheme } from "./src/brand/styles/theme"
+
+// document badge and action
+import { LiveURLBadge } from "./src/badges/LiveURLBadge"
+import { SectionBadge } from "./src/badges/sectionBadge"
+import { ResolveDocumentActions } from "./src/documentActions"
+
+// schemas
+import { schemaTypes } from "./schemas/schema"
+
+// plugins
+import { visionTool } from "@sanity/vision"
 import { media, mediaAssetSource } from "sanity-plugin-media"
 import { webriqBlog } from "@webriq-pagebuilder/sanity-plugin-webriq-blog"
 import { webriqForms } from "@webriq-pagebuilder/sanity-plugin-webriq-forms"
-import { DefaultStudioTheme } from "./src/brand/styles/theme"
-import { ResolveDocumentActions } from "./src/documentActions"
+import { webriqPayments } from "@webriq-pagebuilder/sanity-plugin-webriq-payments"
 
 
 export default defineConfig({
   title: SANITY_STUDIO_PROJECT_NAME,
   projectId: SANITY_STUDIO_API_PROJECT_ID,
   dataset: SANITY_STUDIO_DATASET,
-  plugins: [deskStructure, visionTool(), webriqForms(), webriqBlog(), media()],
+  plugins: [deskStructure, visionTool(), webriqForms(), webriqPayments(), webriqBlog(), media()],
   tools: (prev) => {
     // 👇 Uses environment variables set by Vite in development mode
     if (import.meta.env.DEV) {
