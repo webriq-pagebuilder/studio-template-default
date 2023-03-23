a665247bff35a0b20eab2b4e82181b5410fefef5
Mariel Ann Filosopo <mariel.filosopo@webriq.services>
2023-03-23 14:44:47 +0800

    Update resolvePreviewUrl.ts

--- src/resolvePreviewUrl.ts
+++ src/resolvePreviewUrl.ts
@@ -7,56 +7,35 @@ import type { SanityDocumentLike } from "sanity"
 
 export default function resolveProductionUrl(doc: SanityDocumentLike & { slug?: { current: string } }) {
   const currentSlug = doc?.slug?.current || ""
-  const previewUrl = `api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=${currentSlug}`
 
-  // only show the "Open Preview" option for the following documents
-  if (["page", "post"].includes(doc?._type)) {
-    if (window.location.hostname.includes("localhost")) {
-      return `${SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"}/${previewUrl}`
-    }
+  // localhost
+  if(window.location.hostname.includes("localhost")) {
+    return PreviewURL(SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000", SANITY_STUDIO_PREVIEW_SECRET, doc?._type, currentSlug)
+  }
 
-    return `${SANITY_STUDIO_PRODUCTION_SITE_URL}/${previewUrl}`
-  } else if (doc?._type === "mainProduct") {
-    if (window.location.hostname.includes("localhost")) {
-      return `${
-        SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"
-      }/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&type=products&slug=${currentSlug}`
-    }
+  // live site
+  return PreviewURL(SANITY_STUDIO_PRODUCTION_SITE_URL || "http://localhost:3000", SANITY_STUDIO_PREVIEW_SECRET, doc?._type, currentSlug)
+}
 
-    return `${SANITY_STUDIO_PRODUCTION_SITE_URL}/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&type=products&slug=${currentSlug}`
-  } else if (doc?._type === "mainCollection") {
-    if (window.location.hostname.includes("localhost")) {
-      return `${
-        SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"
-      }/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&type=collections&slug=${currentSlug}`
-    }
-
-    return `${SANITY_STUDIO_PRODUCTION_SITE_URL}/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&type=collections&slug=${currentSlug}`
-  } else if (doc?._type === "cartPage") {
-    if (window.location.hostname.includes("localhost")) {
-      return `${
-        SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"
-      }/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=cart`
-    }
-
-    return `${SANITY_STUDIO_PRODUCTION_SITE_URL}/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=cart`
-  } else if (doc?._type === "wishlistPage") {
-    if (window.location.hostname.includes("localhost")) {
-      return `${
-        SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"
-      }/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=wishlist`
-    }
-
-    return `${SANITY_STUDIO_PRODUCTION_SITE_URL}/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=wishlist`
-  } else if (doc?._type === "searchPage") {
-    if (window.location.hostname.includes("localhost")) {
-      return `${
-        SANITY_STUDIO_DEV_SITE_URL || "http://localhost:3000"
-      }/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=search`
-    }
-
-    return `${SANITY_STUDIO_PRODUCTION_SITE_URL}/api/preview?secret=${SANITY_STUDIO_PREVIEW_SECRET}&slug=search`
+// only show the "Open Preview" option for the following documents
+function PreviewURL(siteUrl: string, previewSecret: string, documentType: string, slug: string) {
+  if (["page", "post"].includes(documentType)) {
+    return `${BaseUrl(siteUrl, previewSecret)}&slug=${slug}`
+  } else if (documentType === "mainProduct") {
+    return `${BaseUrl(siteUrl, previewSecret)}&type=products&slug=${slug}`
+  } else if (documentType === "mainCollection") {
+    return `${BaseUrl(siteUrl, previewSecret)}&type=collections&slug=${slug}`
+  } else if (documentType === "cartPage") {
+    return `${BaseUrl(siteUrl, previewSecret)}&slug=cart`
+  } else if (documentType === "wishlistPage") {
+    return `${BaseUrl(siteUrl, previewSecret)}&slug=wishlist`
+  } else if (documentType === "searchPage") {
+    return `${BaseUrl(siteUrl, previewSecret)}&slug=search`
   }
 
   return undefined
+}
+
+function BaseUrl (siteUrl: string, previewSecret: string) {
+  return `${siteUrl}/api/preview?secret=${previewSecret}&source=studio`
 }
